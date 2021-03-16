9f81c9f3b467b766de6fd406b22d62cf6f19a74e
JSFun9888 <madcatz9888@hotmail.com>
2021-03-16 18:21:49 +1100

    added buffered output and input to correctly send the data, input still needs work but sending ok now

--- Scheduler/Client-Job-Scheduler/src/ClientJobScheduler.java
+++ Scheduler/Client-Job-Scheduler/src/ClientJobScheduler.java
@@ -3,44 +3,67 @@ import java.io.*;
 
 public class ClientJobScheduler {
   public static String HELO = "HELO";
-  public static String AUTH = "AUTH xxx";
+  public static String AUTH = "AUTH alpha";
   public static String REDY = "REDY";
   public static String QUIT = "QUIT";
+  public static char[] HI = {'H','E','L','O'};
   
   public static void main (String args []){
     try{
       Socket s = new Socket("127.0.0.1", 50000);
       DataInputStream din = new DataInputStream(s.getInputStream());
       DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+      BufferedOutputStream bout = new BufferedOutputStream(dout);
+      BufferedInputStream bin = new BufferedInputStream(din);
       String str = "";
+      System.out.println("connected");
+
+      
 
       //send HELO mesg
-      dout.writeUTF("HELO");
-      dout.flush();
-      //read the ok msg
-      str = (String)din.readUTF();
-      System.out.print(str); 
-      //send Auth msg? used my name for now
-      dout.writeUTF("AUTH Jaime");
-      dout.flush();
-      //read the reply
-      str = (String)din.readUTF();
-      System.out.print(str); 
+      //dout.writeBytes(HELO);
+      bout.write(HELO.getBytes());
+      System.out.println("Sent HELO");
+      bout.flush();
+
+      //read the reply for HELO
+      int read = bin.read();
+      System.out.println(read); 
+
+      //send Auth msg to server
+      bout.write(AUTH.getBytes());
+      bout.flush();
+
+      //read the reply AUTH
+      read = bin.read();
+      System.out.println(read);
+      read = bin.read();
+      System.out.println(read); 
+      
       //send REDY msg
-      dout.writeUTF("REDY");
-      dout.flush();
-      //read the reply
-      str = (String)din.readUTF();
-      System.out.print(str); 
+      bout.write(REDY.getBytes());
+      bout.flush();
+
+      //read the reply to REDY
+      read = bin.read();
+      System.out.println(read); 
+
       //tell server to quit
-      dout.writeUTF("QUIT");
-      dout.flush();
+      bout.write(QUIT.getBytes());
+      bout.flush();
+      
       //read reply
       str = (String)din.readUTF();
-      System.out.print(str); 
+      System.out.println(str); 
+
+      bout.close();
+      dout.close();
+      s.close();
       
     } catch(Exception e){
       System.out.println(e);
     }
+
+    
   }
 }
