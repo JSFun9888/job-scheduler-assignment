090d44f88cb51ab2df262b1ad1d86c6cff6f8e2b
JSFun9888 <madcatz9888@hotmail.com>
2021-03-24 01:48:02 +1100

    GETS msg is sent, the server info is split and a job is scheduled by hardcoding

--- Scheduler/Client-Job-Scheduler/src/ClientJobScheduler.java
+++ Scheduler/Client-Job-Scheduler/src/ClientJobScheduler.java
@@ -13,7 +13,8 @@ public class ClientJobScheduler {
   public static String AUTH = "AUTH alpha";
   public static String REDY = "REDY";
   public static String QUIT = "QUIT";
-  public static char[] HI = {'H','E','L','O'};
+  public static char[] HI = {'H','E','L','O'};//don't need
+  
 
   public ClientJobScheduler(){
     
@@ -70,6 +71,37 @@ public class ClientJobScheduler {
       //read the reply to REDY
       serverReply = cjs.readMsg(new byte[32], bin);
       System.out.println("RCVD in response to REDY: " + serverReply);  
+
+      //new stuff for wk 5 prac
+      //send GETS msg new stuff here
+      bout.write("GETS All".getBytes());
+      bout.flush();
+      serverReply = cjs.readMsg(new byte[100], bin);
+      System.out.println("RCVD in response to GETS All: " + serverReply);
+      bout.write("OK".getBytes());
+      bout.flush();
+      serverReply = cjs.readMsg(new byte[100], bin); //get all the server info
+      String[] arrOfStr = serverReply.split("\n"); //split the response into arr of strings
+      System.out.println("RCVD in response to ok: " + serverReply);
+      bout.write("OK".getBytes());
+      bout.flush();
+      serverReply = cjs.readMsg(new byte[100], bin); 
+      System.out.println("RCVD in response to ok: " + serverReply);//end of GETS
+
+      bout.write("SCHD 0 joon 0".getBytes()); //hard code of SCHD job 0 to server joon 0
+      bout.flush();
+      serverReply = cjs.readMsg(new byte[100], bin);
+      System.out.println("RCVD in response to SCHD: " + serverReply);
+
+      //send REDY msg
+      bout.write(REDY.getBytes()); //2nd job dispatch
+      bout.flush();
+      //read the reply to REDY
+      serverReply = cjs.readMsg(new byte[32], bin);
+      System.out.println("RCVD in response to REDY: " + serverReply); 
+      //end of new stuff for wk 5 prac
+      //need a loop for REDY
+      //Also need to store server and job info
 
       //tell server to quit
       bout.write(QUIT.getBytes());
